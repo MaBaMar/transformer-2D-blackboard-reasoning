52a067eace9e7b4e0e6d014cf51689804f1a9d99
Bongni <s.bongni@bluewin.ch>
2025-11-13 11:52:44 +1100

    Add tokenizer arg for base and scratchpad.

--- projectlib/datagen.py
+++ projectlib/datagen.py
@@ -29,15 +29,19 @@ BB_OPERATION = Addition()
 TRAIN_PATH_BASE = "datasets/{}_train.pt"
 EVAL_PATH_BASE = "datasets/{}_eval.pt"
 
+TOKENIZERS = {
+    "None": None,
+    "bert": "bert-base-uncased",
+    "t5": "t5-small",
+}
+
 
 #
 #   Functions that generate train/eval datasets of each type
 #
 
 
-def generate_addition(digits: int, low: int, high: int):
-    tokenizer = AutoTokenizer.from_pretrained("t5-small")
-
+def generate_addition(digits: int, low: int, high: int, tokenizer: AutoTokenizer=None):
     AdditionDataset(
         path=EVAL_PATH_BASE.format(f"addition_{digits}"),
         tokenizer=tokenizer,
@@ -48,9 +52,7 @@ def generate_addition(digits: int, low: int, high: int):
     )
 
 
-def generate_scratchpad(digits: int, low: int, high: int):
-    tokenizer = AutoTokenizer.from_pretrained("t5-small")
-
+def generate_scratchpad(digits: int, low: int, high: int, tokenizer: AutoTokenizer=None):
     ScratchpadDataset(
         path=EVAL_PATH_BASE.format(f"scratchpad_{digits}"),
         tokenizer=tokenizer,
@@ -61,7 +63,7 @@ def generate_scratchpad(digits: int, low: int, high: int):
     )
 
 
-def generate_blackboard(digits: int, low: int, high: int):
+def generate_blackboard(digits: int, low: int, high: int, tokenizer: AutoTokenizer=None):
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
     tokenizer.add_special_tokens({'pad_token': BB_PAD_TOKEN, 'sep_token': BB_ROW_SEP_TOKEN})
 
@@ -98,21 +100,24 @@ def generate_blackboard(digits: int, low: int, high: int):
 #
 
 
-def main(digits: int):
+def main(digits: int, tokenizer_name: str):
     low = 10 ** (digits - 1)
     high = 10 ** digits
 
-    print(f"Generating datasets with {digits}-digit numbers in the range [{low}, {high}) ...")
+    print(f"Generating datasets with {digits}-digit numbers in the range [{low}, {high}) using tokenizer: {tokenizer_name}.")
 
-    generate_addition(digits, low, high)
-    generate_scratchpad(digits, low, high)
-    generate_blackboard(digits, low, high)
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZERS[tokenizer_name]) if tokenizer_name != "None" else None
+
+    generate_addition(digits, low, high, tokenizer=tokenizer)
+    generate_scratchpad(digits, low, high, tokenizer=tokenizer)
+    generate_blackboard(digits, low, high, tokenizer=None)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--digits", type=int, help="Amount of digits each number has.")
+    parser.add_argument("--tokenizer", type=str, default="None", help="Tokenizer to use (default: None).")
 
     args = parser.parse_args()
 
-    main(digits=args.digits)
+    main(digits=args.digits, tokenizer_name=args.tokenizer)
