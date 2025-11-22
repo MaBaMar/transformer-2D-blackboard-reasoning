d950b726a1ddf708bcf53705fa303aa285c647b0
Marco Baumann <mbaumann43@hotmail.com>
2025-11-22 11:33:36 +0100

    add source structure for model implementation

---
+++ src/models/edgar.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------
+# edgar.py
+#
+# Encoder-Decoder Generative Algorithmic Reasoner
+#
+# Purpose: Full model implementation based on 2D rope and autoregressive reasoning step
+# generation for additions and subtractions
+#
+# Name: Toy name, feel free to change it to sth fancy
+# ------------------------------------------------------------
