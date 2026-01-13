9ebd4ceff487824173fb4b1623558e657bb9f26c
Marco Baumann <mbaumann43@hotmail.com>
2026-01-13 14:13:44 +0100

    clean some more files

--- projectlib/plotter.py
+++
@@ -1,4 +0,0 @@
-"""
-Library for reusable plotting functionality for the research project. Feel free to define new functions here and to use them in the code.
-Please regularly push updated versions of the library, so others can use the same functionality.
-"""
